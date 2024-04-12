85125cacf4bed8c30edae262f11b8a3ecdf12dbf
Marius Santiago Wilsch <84270258+VerdantStartup@users.noreply.github.com>
2024-04-12 17:12:50 +0300

    now saving to superbase bucket instead of to local disk

--- src/utilities/gen_result.py
+++ src/utilities/gen_result.py
@@ -1,4 +1,5 @@
 import json, os, marvin
+import streamlit as st
 
 
 # * From imports
@@ -7,8 +8,14 @@ from typing import List
 from pprint import pprint
 from extras.prompts import RESULT_PROMPT
 from .call_llm import call_llm
+from st_supabase_connection import SupabaseConnection
 
 from config import total_tokens_used
+
+st_supabase_client = st.connection(
+    name="supabase_prod_connection",
+    type=SupabaseConnection,
+)
 
 
 def use_marvin(res_dict: dict, now: datetime) -> dict:
@@ -61,6 +68,38 @@ def save_result_to_disk(processed_data: dict):
         json.dump(processed_data, f, indent=4)
 
 
+def save_result_to_supabase(processed_data: dict):
+    now = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    file_name = f"{now}.json"
+    try:
+        file_content = json.dumps(processed_data).encode("utf-8")
+        try:
+            st_supabase_client.create_signed_upload_url(
+                "chatbot_results",
+                f"results/{file_name}",
+                file_content,
+                content_type="application/json",
+            )
+        except Exception as e:
+            print(f"Error uploading result file: {e}")
+            # Handle the exception or log the error
+
+        try:
+            st_supabase_client.create_signed_upload_url(
+                "chatbot_results",
+                "last_result/last_result.json",
+                file_content,
+                content_type="application/json",
+            )
+        except Exception as e:
+            print(f"Error uploading last result file: {e}")
+            # Handle the exception or log the error
+
+    except Exception as e:
+        print(f"Error in save_result_to_supabase: {e}")
+        # Handle the exception or log the error
+
+
 def generate_final_result(messages: List, client):
     print("Generating final result")
     data = call_llm(
@@ -72,8 +111,9 @@ def generate_final_result(messages: List, client):
     #! For debugging purposes only - Remove this later. Save the model dump to a file
     now = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
     processed_data = use_marvin(data, now)
-    cost_data = calc_cost(total_tokens_used)
-    processed_data["cost"] = cost_data
+    processed_data["cost"] = calc_cost(total_tokens_used)
     processed_data["chat_history"] = messages
-    save_result_to_disk(processed_data)
-    return processed_data.pop("cost")
+    # * in dev mode, save the result to disk
+    # save_result_to_disk(processed_data)
+    # * in prod mode, save the result to Supabase
+    save_result_to_supabase(processed_data)
