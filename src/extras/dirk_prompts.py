f232822501e05aef6f020a7c6e2945ad2b7be416
Marius Santiago Wilsch <84270258+VerdantStartup@users.noreply.github.com>
2024-04-23 12:27:07 +0200

    Refactored OPENAI_PROMPT

--- src/extras/dirk_prompts.py
+++ src/extras/dirk_prompts.py
@@ -1,20 +1,29 @@
 OPENAI_PROMPT = """Extract the following information from the given text. If a variable cannot be determined exactly from the given text, state this, NEVER make anything up!
 
-For the variable 'personal injury' (personal injury damage), fill in 'Yes' or 'No' with a very brief and concise reason why and specifically what kind of damage/where. For extra clarity, here is the definition of personal injury damage:
+For extra clarity, here is the definition of personal injury damage:
 
 Definition of personal injury damage: liability law (personal injury law) - physical or mental injury caused by another party that can lead to non-material damages or compensation for pain and suffering.
 
-The JSON should contain the following variables filled in accordingly:
+<response_format>
+The JSON must contain the following variables filled in accordingly:
 {
-"personal_injury": "",
-"claim": "",
-"what_happened": "",
-"how_happened": ""
+"personal_injury": true/false,
+"what_happened": string,
+"how_happened": string
 }
 
-Fill in each variable with as specific an explanation as possible, don't miss any details.
+The "personal_injury" field must be a boolean value indicating whether the user is involved in a personal injury case or not.
 
-ONLY OUTPUT THE JSON OBJECT, NOTHING ELSE!"""
+The "what_happened" field must be a string containing a brief description of the events that led to the injury.
+
+The "how_happened" field must be a string containing an explanation of how the injury occurred.
+
+</response_format>
+
+<notes>
+- Fill in each variable with as specific an explanation as possible, don't miss any details.
+</notes>
+"""
 
 CLAUDE_PROMPT = """You will receive a JSON with a description of what happened. Your task consists of 2 parts:
 Check if there is any personal injury damage. If there is NO such damage, return immediately. In that case, ONLY output a '0'.
